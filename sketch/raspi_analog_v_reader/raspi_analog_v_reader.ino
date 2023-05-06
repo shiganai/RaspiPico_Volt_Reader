573a085fe130eab68c6f95df7dd464920a91969c
shiganai <shiganai.hiro@gmail.com>
2023-05-06 10:03:37 +0900

    [add] sketch for analog reader
    Note

---
+++ sketch/raspi_analog_v_reader/raspi_analog_v_reader.ino
@@ -0,0 +1,21 @@
+// 2021/07/25 imo lab.
+// 2023/05/05 分解能設定追加
+//https://garchiving.com/
+
+// Note: A0 pin read around 0.5V with connecting to nothing.
+
+void setup() {
+  Serial.begin(9600);
+  analogReadResolution(12);
+}
+
+uint16_t raw;
+float v;
+
+void loop() {
+  raw = analogRead(A0);
+  v   = raw * 3.3 / (1 << 12);
+
+  Serial.println(v);
+  delay(10);
+}
