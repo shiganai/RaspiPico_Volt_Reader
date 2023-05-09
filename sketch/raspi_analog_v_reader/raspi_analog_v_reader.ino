f23033ede165ea46898f44659c73988751facff8
shiganai <shiganai.hiro@gmail.com>
2023-05-09 22:34:58 +0900

    [update AVR] installed input_pullup
    not working as I want.
    To be reverted.

--- sketch/raspi_analog_v_reader/raspi_analog_v_reader.ino
+++ sketch/raspi_analog_v_reader/raspi_analog_v_reader.ino
@@ -6,6 +6,7 @@
 
 void setup() {
   Serial.begin(9600);
+  pinMode(A0, INPUT_PULLUP); //https://burariweb.info/electronic-work/arduino-tips/arduino-internal-pullup.html
   analogReadResolution(12);
 }
 
